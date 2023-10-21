5dfcd1a374e6842b2307279da3c8d28c1d71af0e
Sai Krishna <saikrishnabadineedi99@gmail.com>
2023-10-21 13:06:54 -0500

    Front end Customer Registration Form

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
@@ -3,6 +3,9 @@ package com.shopme;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +25,20 @@ public class MainController {
 		model.addAttribute("listCategories", listCategories);
 		return "index";
 	}
-	
+
 //	@GetMapping("/")
 //	public String viewHomePage() {
 //		return "index";
 //	}
-	
-	
+
+	@GetMapping("/login")
+	public String viewLoginPage() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+
+		return "redirect:/";
+	}
+
 }
