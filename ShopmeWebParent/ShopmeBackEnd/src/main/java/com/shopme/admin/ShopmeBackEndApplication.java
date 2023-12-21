2db3121568ae62b7ca7a6e811b608d13042d6357
Sai Krishna <saikrishnabadineedi99@gmail.com>
2023-12-20 21:13:34 -0600

    java 17 version

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -3,17 +3,9 @@ package com.shopme.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.shopme.common.MongodbConfig.MongoMultiConfig;
-
-@SpringBootApplication(scanBasePackages = {"com.shopme.admin","com.shopme.common.MongodbConfig"})
+@SpringBootApplication(scanBasePackages = {"com.shopme.admin","com.shopme.common"})
 @EntityScan({ "com.shopme.common.entity", "com.shopme.admin.user" })
 
 /*
